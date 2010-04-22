8145aa10be0495df030479aa044f293af03f1f8a
Erwan Barrier <erwan.barrier@gmail.com>
2010-04-22 18:32:01 +0200

    add an attribute to set binding address of redis

--- redis/attributes/server.rb
+++ redis/attributes/server.rb
@@ -1,4 +1,5 @@
-set_unless[:redis][:datadir] = "/var/lib/redis"
+set_unless[:redis][:datadir]      = "/var/lib/redis"
+set_unless[:redis][:bind_address] = ipaddress
 
 if attribute?(:ec2)
   set_unless[:redis][:ec2_path] = "/mnt/redis"
